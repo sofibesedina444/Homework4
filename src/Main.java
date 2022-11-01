bb1654b85f02fed0e193fa4d8fbe4ccf642d16fd
salva <salvationordeath@gmail.com>
2022-11-01 21:05:00 +0700

    ДЗ 2 - задачи с 1 по 3

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ДЗ 1 - задача 1:");
-        int humanAge = 13;
+        System.out.println("ДЗ 2 - задача 1:");
+        int humanAge = 25;
         if (humanAge >= 18) {
             System.out.println("Поздравляем с совершеннолетием!");
-        }
-        if (humanAge < 18) {
-            System.out.println("Возраст совершеннолетия ещё не наступил, нужно немного подождать");
+        } else {
+            System.out.println("Возраст совершеннолетия ещё не наступил, нужно немного подождать.");
         }
 
-        System.out.println("ДЗ 1 - задача 2:");
-        if (humanAge >= 7) {
+        System.out.println("ДЗ 2 - задача 2:");
+        if (humanAge >= 7 && humanAge<18) {
             System.out.println("Ребенок ходит в школу");
-        }
-        if (humanAge >= 18) {
+        } else if (humanAge >= 18 && humanAge<24) {
             System.out.println("Человек уже закончил школу и может отправляться в университет");
-        }
-        if (humanAge >= 24) {
+        } else if (humanAge >= 24) {
             System.out.println("Человек окончил университет и ему пора искать первую работу");
         }
 
-        System.out.println("ДЗ 1 - задача 3:");
+        System.out.println("ДЗ 2 - задача 3:");
         int numberOfSeatsInTheCar = 102;
         int numbersOfSeats = 60;
         int numbersOfOther = numberOfSeatsInTheCar-numbersOfSeats;
@@ -28,18 +25,16 @@ public class Main {
         int otherUsed = 37;
         if (seatsUsed < numbersOfSeats) {
             System.out.println("В вагоне осталось "+(numbersOfSeats-seatsUsed)+" сидячих мест");
-        }
-        if (seatsUsed==numbersOfSeats) {
+        } else {
             System.out.println("В вагоне нет сидячих мест");
         }
         if (otherUsed < numbersOfOther) {
             System.out.println("В вагоне осталось "+(numbersOfOther-otherUsed)+" стоячих мест");
-        }
-        if (otherUsed==numbersOfOther) {
+        } else {
             System.out.println("В вагоне нет стоячих мест");
-        if ((seatsUsed+otherUsed) >= numberOfSeatsInTheCar) {
-            System.out.println("В вагоне нет свободных мест");
-        }
+            if ((seatsUsed+otherUsed) >= numberOfSeatsInTheCar) {
+                System.out.println("В вагоне нет свободных мест");
+            }
         }
     }
 }
