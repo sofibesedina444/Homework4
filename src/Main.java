56b14f6bbc68c045659c645496ee049167d2e56b
salva <salvationordeath@gmail.com>
2022-11-01 21:52:07 +0700

    ДЗ 3 - задачи с 1 по 3

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,38 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ДЗ 2 - задача 1:");
-        int humanAge = 25;
-        if (humanAge >= 18) {
-            System.out.println("Поздравляем с совершеннолетием!");
-        } else {
-            System.out.println("Возраст совершеннолетия ещё не наступил, нужно немного подождать.");
+        System.out.println("ДЗ 3 - задача 1:");
+        int humanAge = 15;
+        if (humanAge>=2 && humanAge<7) {
+            System.out.println("Если возраст человека равен "+humanAge+", то ему нужно ходить в детский сад");
+        } else if (humanAge>=7 && humanAge<=18) {
+            System.out.println("Если возраст человека равен "+humanAge+", то ему нужно ходить в школу");
+        } else if (humanAge>18 && humanAge<=24) {
+            System.out.println("Если возраст человека равен "+humanAge+", то ему нужно ходить в университет");
+        } else if (humanAge>24) {
+            System.out.println("Если возраст человека равен "+humanAge+", то ему нужно ходить на работу");
         }
 
-        System.out.println("ДЗ 2 - задача 2:");
-        if (humanAge >= 7 && humanAge<18) {
-            System.out.println("Ребенок ходит в школу");
-        } else if (humanAge >= 18 && humanAge<24) {
-            System.out.println("Человек уже закончил школу и может отправляться в университет");
-        } else if (humanAge >= 24) {
-            System.out.println("Человек окончил университет и ему пора искать первую работу");
+        System.out.println("ДЗ 3 - задача 2:");
+        if (humanAge<=5) {
+            System.out.println("Ребенок не может кататься на аттракционе");
+        } else if (humanAge>5 && humanAge<=14) {
+            System.out.println("Ребенок может кататься на аттракционе только в сопровождении взрослого");
+        } else if (humanAge>14) {
+            System.out.println("Ребенок может кататься на аттракционе без сопровождения взрослого");
         }
 
-        System.out.println("ДЗ 2 - задача 3:");
-        int numberOfSeatsInTheCar = 102;
-        int numbersOfSeats = 60;
-        int numbersOfOther = numberOfSeatsInTheCar-numbersOfSeats;
-        int seatsUsed = 50;
-        int otherUsed = 37;
-        if (seatsUsed < numbersOfSeats) {
-            System.out.println("В вагоне осталось "+(numbersOfSeats-seatsUsed)+" сидячих мест");
+        System.out.println("ДЗ 3 - задача 3:");
+        int one = 10;
+        int two = 20;
+        int three = 30;
+        if (one>two && one>three) {
+            System.out.println(one);
+        } else if (two>one && two>three) {
+            System.out.println(two);
+        } else if (three>one && three>two) {
+            System.out.println(three);
         } else {
-            System.out.println("В вагоне нет сидячих мест");
-        }
-        if (otherUsed < numbersOfOther) {
-            System.out.println("В вагоне осталось "+(numbersOfOther-otherUsed)+" стоячих мест");
-        } else {
-            System.out.println("В вагоне нет стоячих мест");
-            if ((seatsUsed+otherUsed) >= numberOfSeatsInTheCar) {
-                System.out.println("В вагоне нет свободных мест");
-            }
+            System.out.println("Числа равны");
         }
     }
 }
